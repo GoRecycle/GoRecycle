b8d0cc699dd9b166f529dacdfdab0782071a2dad
NatanaelAnantaSatrio <46699826+NatanaelAnantaSatrio@users.noreply.github.com>
2020-03-08 02:57:05 +0800

    z

--- webpage/appointment.php
+++ webpage/appointment.php
@@ -65,19 +65,7 @@ include '../php/dbConnection.php';
 		<div class="container">
 			<h1>HOME Recycler</h1>
 		</div>
-<div class="container">
-  <div class="col-md-4">
-  <div class="card" style="width: 18rem;">
 
-<div class="card-body">
-<h5 class="card-title">Card title</h5>
-<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-<a href="#" class="card-link">Card link</a>
-<a href="#" class="card-link">Another link</a>
-</div>
-</div>
-</div>
 
 <?php
 $query = "SELECT * FROM material";
